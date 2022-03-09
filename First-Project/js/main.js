fcbfd9ccd5cfe70ffae4a5b5355906e5105b8cd8
OscarFelixEscobar <oscarfelixescobar@gmail.com>
2022-03-09 15:25:56 -0800

    Add files via upload

---
+++ First-Project/js/main.js
@@ -0,0 +1,30 @@
+/*----- constants -----*/
+
+
+
+
+
+
+/*----- app's state (variables) -----*/
+
+
+
+
+
+
+
+/*----- cached element references -----*/
+
+let squares = document.getElementsByClassName("block")
+
+
+
+
+
+/*----- event listeners -----*/
+
+
+
+
+
+/*----- functions -----*/
